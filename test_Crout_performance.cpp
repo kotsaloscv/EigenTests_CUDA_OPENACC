8a2f83806f5ff0fd2ed4fd96293f4373f49ccb1e
Christos Kotsalos <christos.kotsalos@epfl.ch>
2021-06-02 17:27:33 +0200

    WIP

--- test_Crout_performance.cpp
+++ test_Crout_performance.cpp
@@ -5,14 +5,14 @@
 #include <chrono>
 #include <vector>
 
-#include <omp.h>
-
 #include "Eigen/Dense"
 #include "Eigen/LU"
 
 using namespace Eigen;
 using namespace std;
 
+#define DIM 4
+#define LOOPS 50000
 
 /**
  * \brief Crout matrix decomposition : LU Decomposition of (S)ource matrix stored in (D)estination matrix.
@@ -86,73 +86,64 @@ bool allclose(const Eigen::DenseBase<DerivedA>& a,
 template<typename T>
 bool test_Crout_performance(T rtol = 1e-6, T atol = 1e-6) 
 {
-    std::vector<Matrix<T, Dynamic, Dynamic, Eigen::RowMajor>> A_;
-    std::vector<Matrix<T, Dynamic, 1>> b_;
-    std::vector<Matrix<T, Dynamic, 1>> x_eigen_;
-    std::vector<Matrix<T, Dynamic, 1>> x_crout_;
+    using MatType = Matrix<T, DIM, DIM, Eigen::RowMajor>;
+    using VecType = Matrix<T, DIM, 1>;
 
     std::random_device rd; // seeding
     std::mt19937 mt(rd());
     std::uniform_real_distribution<T> nums(-1, 1);
     
-    for (int mat_size = 2; mat_size <= 10; mat_size++)
+    MatType A[LOOPS];
+    VecType b[LOOPS], x_eigen[LOOPS], x_crout[LOOPS];
+
+    for (int i = 0; i < LOOPS; i++)
     {
-        Matrix<T, Dynamic, Dynamic, Eigen::RowMajor> A(mat_size, mat_size);
-        Matrix<T, Dynamic, 1> b(mat_size);
-        
-        for (int i = 0; i < 100000; ++i) 
-        {
-            // initialization
-            for(int i = 0; i <  mat_size; i++) {
-                for(int j = 0; j < mat_size; j++) {
-                    A(i,j) = nums(mt);
-                    b(i) = nums(mt);
+        MatType A_;
+        VecType b_, x_eigen_, x_crout_;
+        do {
+            for(int i = 0; i <  DIM; i++) {
+                for(int j = 0; j < DIM; j++) {
+                    A_(i,j) = nums(mt);
+                    b_(i) = nums(mt);
+                    
+                    x_eigen_(i) = (T)0;
+                    x_crout_(i) = (T)0;
                 }
             }
-
-            // Checking Invertibility
-            if (!A.fullPivLu().isInvertible())
-                continue;
-
-            A_.push_back(A);
-            b_.push_back(b);
-            x_eigen_.push_back(b); // just an initialization
-            x_crout_.push_back(b); // just an initialization
         }
+        while (!A_.fullPivLu().isInvertible()); // Checking Invertibility
+
+        A[i] = A_;
+        b[i] = b_;
+
+        x_eigen[i] = x_eigen_;
+        x_crout[i] = x_crout_;
     }
 
     // Eigen CPU
     std::chrono::duration<double> eigen_solve(std::chrono::duration<double>::zero());
     auto t1 = std::chrono::high_resolution_clock::now();
     #pragma omp parallel for
-    for (int i = 0; i < A_.size(); i++)
+    for (int i = 0; i < LOOPS; i++)
     {
-        x_eigen_[i] = A_[i].partialPivLu().solve(b_[i]);
+        x_eigen[i] = A[i].partialPivLu().solve(b[i]);
     }
     auto t2 = std::chrono::high_resolution_clock::now();
     eigen_solve = (t2 - t1);
     cout << "Eigen : " << eigen_solve.count()*1e3 << " ms" << endl;
 
     // Crout
-    std::chrono::duration<double> crout_solve(std::chrono::duration<double>::zero());
-    t1 = std::chrono::high_resolution_clock::now();
-    #pragma omp parallel for
-    for (int i = 0; i < A_.size(); i++)
+    #pragma acc parallel loop copyin(A[0:LOOPS], b[0:LOOPS]) copy(x_crout[0:LOOPS])
+    for (int i = 0; i < LOOPS; i++)
     {
-        int mat_size = b_[i].size();
-        Matrix<T, Dynamic, Dynamic, Eigen::RowMajor> LU(mat_size, mat_size);
-        Crout<T>(mat_size, A_[i].data(), LU.data());
-        solveCrout<T>(mat_size, LU.data(), b_[i].data(), x_crout_[i].data());
+        Crout<T>(DIM, A[i].data(), A[i].data()); // in-place LU decomposition
+        solveCrout<T>(DIM, A[i].data(), b[i].data(), x_crout[i].data());
     }
-    t2 = std::chrono::high_resolution_clock::now();
-    crout_solve = (t2 - t1);
-    cout << "Crout : " << crout_solve.count()*1e3 << " ms" << endl;
 
-    // Check correctness
-    for (int i = 0; i < A_.size(); i++) {
-        if (!allclose(x_eigen_[i], x_crout_[i], rtol, atol))
+    // Check Correctness
+    for (int i = 0; i < LOOPS; i++)
+       if (!allclose(x_eigen[i], x_crout[i], rtol, atol))
             return false;
-    }
 
     return true;
 }
